585cc0e9b705c5bd6309af08fdea9dba74105779
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-11-15 20:22:26 +0100

    Use multiple tokens if provided :-)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import colors from "colors";
 const env = JSON.parse(fs.readFileSync(".env.json"));
 const logfile = env["logfile"] ?? "./journal.log";
 
-const myToken = env["tokens"][0];
+const tokens = env["tokens"];
 
 const pixelsDepartement = JSON.parse(
 	fs.readFileSync("./data/desert-de-lest-pixels.json")
@@ -20,9 +20,14 @@ for (const p of pixelsDepartement) {
 
 	const wantedColor = "#D09E3C";
 
-	const currentColor = p.hexColor; // await getColorPixel(p);
+	const currentColor = p.hexColor;
 	if (currentColor === wantedColor) {
 		console.info(`good color ${currentColor}!`.green);
+		continue;
+	}
+
+	if (wantedColor == await getColorPixel(p)) {
+		console.info(`good color ${currentColor}! (but think to npm run update-data sometimes)`.green);
 		continue;
 	}
 
@@ -56,39 +61,57 @@ async function getColorPixel(pixel) {
 }
 
 async function updatePixel(pixel, newColor) {
-	const entityId = mapEntityIds[pixel.indexInFlag];
+	const request = {
+		hexColor: newColor,
+		pixelId: mapEntityIds[pixel.indexInFlag],
+	};
 
-	const request = { hexColor: newColor, pixelId: entityId };
-	const config = { headers: { Authorization: myToken } };
+	let remainingTokens = tokens;
 
-	try {
-		const answer = await axios.put(
-			"https://api-flag.fouloscopie.com/pixel",
-			request,
-			config
-		);
+	while (true) {
+		const config = { headers: { Authorization: remainingTokens[0] } };
 
-		return answer.status;
-	} catch (err) {
-		if (err.response.data.message === "Please retry later.") {
-			const cooldown = err.response.data.retryAfter + 200;
-			process.stdout.write(`Waiting ${cooldown} for cooldown...`.cyan);
-			await timer(cooldown);
+		try {
+			const answer = await axios.put(
+				"https://api-flag.fouloscopie.com/pixel",
+				request,
+				config
+			);
 
-			try {
-				const newAnswer = await updatePixel(pixel, newColor);
-				return newAnswer.status;
-			} catch (newErr) {
-				err = newErr;
+			return answer.status;
+		} catch (err) {
+			if (err.response?.data?.message === "Please retry later.") {
+				const cooldown = err.response.data.retryAfter + 200;
+
+				if (remainingTokens.length > 1) {
+					process.stdout.write(`Cooldown of ${cooldown}, trying with another token...`.cyan);
+					remainingTokens = remainingTokens.slice(1)
+					continue;
+				}
+
+				// Waiting and retrying
+				process.stdout.write(`No other token, waiting ${cooldown} for cooldown...`.cyan);
+				await timer(cooldown);
+
+				try {
+					const newAnswer = await updatePixel(pixel, newColor);
+					return newAnswer.status;
+				} catch (newErr) {
+					err = newErr;
+				}
 			}
-		}
 
-		fs.writeFile(
-			logfile,
-			`Error when updating [${pixel.x}, ${pixel.y}] (${err.response.status
-			}): ${JSON.stringify(err.response.data)}\n`,
-			{ flag: "a+" },
-			(err) => { }
-		);
+			let message = err;
+			if (err.response?.data) {
+				message = `(${err.response.status}) ${JSON.stringify(err.response.data)}`;
+			}
+
+			fs.writeFile(
+				logfile,
+				`Error when updating [${pixel.x}, ${pixel.y}]: ${message}\n`,
+				{ flag: "a+" },
+				(err) => { }
+			);
+		}
 	}
 }
